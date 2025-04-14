44018080b664571d7c29c2f409e43517dfa807c6
John Orford <john.orford@siemens.com>
2025-04-14 14:52:22 +0200

    Set InitialPacketSize to 1262 to accommodate MTU constraints

--- listeners.go
+++ listeners.go
@@ -467,6 +467,7 @@ func (na NetworkAddress) ListenQUIC(ctx context.Context, portOffset uint, config
 			&quic.Config{
 				Allow0RTT: true,
 				Tracer:    qlog.DefaultConnectionTracer,
+				InitialPacketSize: 1262,
 			},
 		)
 		if err != nil {
